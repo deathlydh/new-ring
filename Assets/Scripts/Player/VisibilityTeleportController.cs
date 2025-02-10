9a567086a2f63903cdcf0f77e6abad2c787aea16
German <gchistopashin@mail.ru>
2025-02-10 15:41:26 +0500

    add zombi player

---
+++ Assets/Scripts/Player/VisibilityTeleportController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class VisibilityTeleportController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject lController, rController;
+    [SerializeField]
+    private InputActionProperty left, right;
+    [SerializeField]
+    private Animator lAnimator;
+    [SerializeField]
+    private InputActionProperty ltrigger, lcuroc;
+    [SerializeField]
+    private Animator rAnimator;
+    [SerializeField]
+    private InputActionProperty rtrigger, rcuroc;
+
+    public bool GetIsTeleport()
+    {
+        return isTeleport;
+    }
+    public void SetIsTeleport(bool value)
+    {
+        isTeleport = value;
+    }
+    public bool isTeleport;
+
+    void Update()
+    {
+        if (isTeleport)
+        {
+            lController.SetActive(left.action.ReadValue<Vector2>().y > .1f);
+            rController.SetActive(right.action.ReadValue<Vector2>().y > .1f);
+        }
+        
+            float l1 = ltrigger.action.ReadValue<float>();
+            float l2 = lcuroc.action.ReadValue<float>();
+            lAnimator.SetFloat("triger", l1);
+            lAnimator.SetFloat("curoc", l2);
+
+            float r1 = rtrigger.action.ReadValue<float>();
+            float r2 = rcuroc.action.ReadValue<float>();
+            rAnimator.SetFloat("triger", r1);
+            rAnimator.SetFloat("curoc", r2);
+    }
+}
