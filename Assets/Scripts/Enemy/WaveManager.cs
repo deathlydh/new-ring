0ea22d2a592599b1a7e3f6445413bc5597f81f56
Artem <mr.artem1056@gmail.com>
2025-03-11 19:16:37 +0500

    WaveManager fix

--- Assets/Scripts/Enemy/WaveManager.cs
+++ Assets/Scripts/Enemy/WaveManager.cs
@@ -5,13 +5,13 @@ using System;
 
 public class WaveManager : MonoBehaviour
 {
-    [SerializeField] private WaveConfig[] waveConfigs;  // Массив WaveConfig для всех волн
+    [SerializeField] private WaveConfig[] waveConfigs;  
     [SerializeField] private EnemySpawner enemySpawner;
-    [SerializeField] private float waveInterval = 5f; // Задержка между волнами
+    [SerializeField] private float waveInterval = 5f; 
 
-    public event Action<int, int> OnWaveUpdated; // Кол-во врагов, номер волны
+    public event Action<int, int> OnWaveUpdated;
 
-    private int currentWaveIndex = 0; // Индекс текущей волны
+    private int currentWaveIndex = 0; 
     private int enemiesRemaining = 0;
 
     private void Start()
@@ -21,15 +21,15 @@ public class WaveManager : MonoBehaviour
 
     private IEnumerator StartNextWave()
     {
-        // Будем ждать завершения текущей волны, если волна не завершена
+        
         while (currentWaveIndex < waveConfigs.Length)
         {
-            // Получаем количество врагов для текущей волны из массива
+            
             WaveConfig currentWaveConfig = waveConfigs[currentWaveIndex];
             enemiesRemaining = currentWaveConfig.enemiesToSpawn;
             OnWaveUpdated?.Invoke(enemiesRemaining, currentWaveIndex + 1);
 
-            // Спавним врагов текущей волны
+            
             enemySpawner.SpawnWave(currentWaveConfig, OnEnemyDestroyed);
 
             // Ждём, пока все враги будут уничтожены
@@ -48,6 +48,6 @@ public class WaveManager : MonoBehaviour
         enemiesRemaining--;
         OnWaveUpdated?.Invoke(enemiesRemaining, currentWaveIndex);
 
-        // Проверка на завершение волны уже происходит через WaitUntil в StartNextWave
+        
     }
 }
